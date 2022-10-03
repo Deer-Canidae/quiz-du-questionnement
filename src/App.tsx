fddbedc2a97b33a2fa840304ae9fd1b69f5cac4e
Deer-Canidae <deer.canidae@gmail.com>
2022-10-03 09:08:03 -0400

    Made into a PWA

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,32 @@ import Quizz from "./Quizz";
 import arrayShuffle from "./arrayShuffle";
 
 const questions: quizz[] = rawQuestions;
+let installEvent: Event;
 
 export default function App() {
   const [quizzState, setQuizzState] = React.useState(0); // 0 = main menu, 1 Game running
+  const [installable, setInstallable] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener("beforeinstallprompt", function (ev) {
+      ev.preventDefault();
+      setInstallable(true);
+    }, { once: true });
+  }, []);
+
+  const install = function (ev: React.MouseEvent) {
+    ev.preventDefault();
+    if (!installable)
+      return;
+    //@ts-expect-error
+    installEvent.prompt();
+  }
 
   if (quizzState === 0)
     return (
       <>
         <div className="container">
-          <h1>Quiz du questionnement</h1>
+          <h1 onClick={install}>Quiz du questionnement</h1>
           <button
             className="play"
             onClick={() => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "inline",
+      manifest: {
+        background_color: "#eeeeee",
+        theme_color: "#eeeeee",
+        icons: [
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        lang: "fr-CA",
+        short_name: "Quiz Questionnement",
+        name: "Quiz du Questionnement",
+        display: "standalone",
+        description: "Un quiz pour questionner son identité et ses attractions",
+        start_url: "/",
+      },
+    }),
+  ],
+});
